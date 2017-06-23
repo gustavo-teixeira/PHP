23fdf3d06e86e3318ca5c1d893b52ecfac73ecfb
Gustavo Teixeira Ignácio <gustavo_teixeiragti@hotmail.com>
2017-06-22 21:39:17 -0300

    Add files via upload

--- update.php
+++ update.php
@@ -8,13 +8,15 @@ if (!$db) die (' database not selected: '.mysql_error());
 
 $select = "select * from filmes";
 
-#$id = $_GET['id'];
+$id = $_POST['id'];
 
 $nome = $_POST["nome"];
 $diretor = $_POST["diretor"];
 $genero = $_POST["genero"];
 
-if (msql_query("UPDATE filmes SET nome='$nome', diretor='$diretor', genero='$genero' WHERE id_filmes = $id")) {
+$query = "UPDATE filmes SET nome='$nome', diretor='$diretor', genero='$genero' WHERE id_filmes = $id";
+
+if (msql_query($query)) {
 	echo "Filme alterado com sucesso!";
     exit;
 }else{
